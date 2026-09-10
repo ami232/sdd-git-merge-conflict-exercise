1a56fc05c42272ac0500ca381a2158c73347fbbf
Alexandra Molins <alemolgut@gmail.com>
2026-09-10 17:02:28 +0200

    Feature A:Add loyalty discount

--- store.py
+++ store.py
@@ -11,7 +11,10 @@ def calculate_total(
     total = subtotal
 
     # TODO: apply your assigned feature's pricing rule here, gated behind
-    # its argument (apply_discount, apply_tax, or apply_shipping)
+    # its argument (apply_discount, apply_tasx, or apply_shipping)
+    if apply_discount:
+        if subtotal >50:
+            total *=0.9
 
     return total
 
